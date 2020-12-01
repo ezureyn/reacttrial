512abc702475c92d8600dcca3f4aa1fd7ee9209b
ezureyn <rn.erdi@gmail.com>
2020-12-02 02:40:38 +0300

    Updates

---
+++ precache-manifest.5e7e7b2d9869905740853199ca02cfe3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c72f86e3e498ddfdda899c4d4a22e35",
+    "url": "/reacttrial/index.html"
+  },
+  {
+    "revision": "a40eaa88c486bf2ce17a",
+    "url": "/reacttrial/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "205ddad48a40b4128aad",
+    "url": "/reacttrial/static/css/main.446c1d1d.chunk.css"
+  },
+  {
+    "revision": "a40eaa88c486bf2ce17a",
+    "url": "/reacttrial/static/js/2.707a5b1c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reacttrial/static/js/2.707a5b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "205ddad48a40b4128aad",
+    "url": "/reacttrial/static/js/main.f0c5db56.chunk.js"
+  },
+  {
+    "revision": "874c3388be4ddfb912af",
+    "url": "/reacttrial/static/js/runtime-main.e8325512.js"
+  },
+  {
+    "revision": "c2855dc2e6cd1acb92d70a25313f7d46",
+    "url": "/reacttrial/static/media/Atatürk.c2855dc2.jpg"
+  }
+]);
